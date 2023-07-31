e2edde8cc8c6bb4f78f6829b6bdc0b0c26ad0854
SedrlaP <prokop.sedrla@gmail.com>
2023-07-31 18:15:37 +0200

    create ServiceBlob component for homepage

--- src/components/ServiceBlob/ServiceBlob.jsx
+++ src/components/ServiceBlob/ServiceBlob.jsx
@@ -1,27 +1,42 @@
 import "./serviceblob.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faWrench,
+  faCar,
+  faCarBurst,
+  faCarSide,
+} from "@fortawesome/free-solid-svg-icons"
 
 function ServiceBlob() {
+  const style = {
+    color: "#ededed",
+    position: "absolute",
+    top: "-25px",
+    height: "3rem",
+  }
   return (
     <div className="services-container wrapper">
       <div className="blob">
-        <h3>Card 1</h3>
-        <p>Some text</p>
-        <p>Some text</p>
+        <FontAwesomeIcon icon={faWrench} style={style} />
+        <h3 className="blob__text">AUTOSERVIS</h3>
       </div>
       <div className="blob">
-        <h3>Card 1</h3>
-        <p>Some text</p>
-        <p>Some text</p>
+        <FontAwesomeIcon icon={faCarSide} style={style} />
+        <h3 className="blob__text">PNEUSERVIS</h3>
       </div>
       <div className="blob">
-        <h3>Card 1</h3>
-        <p>Some text</p>
-        <p>Some text</p>
+        <FontAwesomeIcon icon={faCar} style={style} />
+        <h3 className="blob__text">
+          AUTO <br />
+          PŮJČOVNA
+        </h3>
       </div>
       <div className="blob">
-        <h3>Card 1</h3>
-        <p>Some text</p>
-        <p>Some text</p>
+        <FontAwesomeIcon icon={faCarBurst} style={style} />
+        <h3 className="blob__text">
+          POJISTNÉ <br />
+          UDÁLOSTI
+        </h3>
       </div>
     </div>
   )
