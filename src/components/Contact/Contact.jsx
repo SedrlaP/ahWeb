10a45e064f53ac91209fdaf7e943b7853468f409
SedrlaP <prokop.sedrla@gmail.com>
2023-08-06 19:01:29 +0200

    add hover animation for links, smaller changes to some components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,17 +5,17 @@ function Contact() {
     <div className="contact wrapper">
       <div className="contact__card">
         <h3 className="contact__card-title">autoservis halas</h3>
-        <ul className="contact__card-list">
-          <li>Adresa: Ulice 1, Město, 624 45</li>
-          <li>Email: email@email.cz</li>
-          <li>Telefon: +420 123 456 789</li>
-          <li>IČO: 124567886765</li>
-          <li>DIČ: 824545676985</li>
+        <ul>
+          <li className="contact__card-text">Adresa: Ulice 1, Město, 624 45</li>
+          <li className="contact__card-text">Email: email@email.cz</li>
+          <li className="contact__card-text">Telefon: +420 123 456 789</li>
+          <li className="contact__card-text">IČO: 124567886765</li>
+          <li className="contact__card-text">DIČ: 824545676985</li>
         </ul>
       </div>
       <div className="contact__card">
         <h3 className="contact__card-title">provozní doba</h3>
-        <p>
+        <p className="contact__card-text">
           Pondělí-Pátek <span>8:00-16:00</span>
         </p>
         <div className="placeholder"></div>
