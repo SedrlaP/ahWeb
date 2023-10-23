338ae9f124c20ac8d051463f5c50e47a5f52236e
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 13:42:14 +0200

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Suspense, lazy } from "react"
 const Home = lazy(() => import("./pages/Home/Home.jsx"))
 const Autoservis = lazy(() => import("./pages/Autoservis/Autoservis.jsx"))
 const Pneuservis = lazy(() => import("./pages/Pneuservis/Pneuservis.jsx"))
-const Autopujcovna = lazy(() => import("./pages/Autopujcovna/Autopujcovna.jsx"))
+//const Autopujcovna = lazy(() => import("./pages/Autopujcovna/Autopujcovna.jsx"))
 const PojistneUdalosti = lazy(() =>
   import("./pages/PojistneUdalosti/PojistneUdalosti.jsx")
 )
@@ -73,6 +73,7 @@ const router = createBrowserRouter([
       },
     ],
   },
+  { basename: "/ahWeb" },
 ])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
