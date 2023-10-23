65dd3a815c88065405f6455c6ac64856a08d8ace
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 15:21:29 +0200

    Updates

---
+++ assets/Hero-054f8200.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-e5c18eb2.js";/* empty css             */function t({title:r,img:e}){return s.jsxs("div",{className:"hero",children:[s.jsx("h2",{className:"hero__title",children:r}),e&&s.jsx("img",{className:"hero__img",src:`ahweb/src/assets/${e}.png`})]})}export{t as H};
