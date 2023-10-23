6eb5d9938ca51e227b2eccd4b3194044e919e439
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 17:01:24 +0200

    Updates

--- assets/Hero-88b90afd.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-7802327b.js";/* empty css             */function t({title:e,img:r}){return s.jsxs("div",{className:"hero",children:[s.jsx("h2",{className:"hero__title",children:e}),r&&s.jsx("img",{className:"hero__img",src:r})]})}export{t as H};
---
+++ assets/autoservis-7dec3802.js
@@ -0,0 +1,1 @@
+const a="/ahWeb/assets/autoservis-a2ac6a69.png";export{a as default};
