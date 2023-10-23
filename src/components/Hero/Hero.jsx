cc6a9f05c9aa1991ac2430a15f7e3453cda9c506
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 15:39:52 +0200

    bug fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero({ title, img }) {
   return (
     <div className="hero">
       <h2 className="hero__title">{title}</h2>
-      {img && <img className="hero__img" src={`ahWeb/src/assets/${img}.png`} />}
+      {img && <img className="hero__img" src={`./${img}.png`} />}
     </div>
   )
 }
