47509de875c53b54609607e98e82cff6868850dd
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 17:27:47 +0200

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,6 @@ import { Suspense, lazy } from "react"
 
 const Home = lazy(() => import("./pages/Home/Home.jsx"))
 const Autoservis = lazy(() => import("./pages/Autoservis/Autoservis.jsx"))
-lazy(() => import("./assets/autoservis.png"))
-lazy(() => import("./assets/lakovna.png"))
-lazy(() => import("./assets/pojistneudalosti.png"))
-lazy(() => import("./assets/pneuservis.png"))
 const Pneuservis = lazy(() => import("./pages/Pneuservis/Pneuservis.jsx"))
 //const Autopujcovna = lazy(() => import("./pages/Autopujcovna/Autopujcovna.jsx"))
 const PojistneUdalosti = lazy(() =>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ahWeb/"
+  base: "/ahWeb/",
+  build: {
+    rollupOptions: {
+      external: ["./assets/pneuservis.png", "./assets/autoservis.png", "./assets/lakovna.png", "./assets/pojstneudalosti.png"]
+    }
+  }
 })
