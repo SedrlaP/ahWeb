6651bd68580e251aeabcfe668969188c672ccb47
SedrlaP <prokop.sedrla@gmail.com>
2023-10-23 13:43:15 +0200

    Updates

--- assets/Autopujcovna-e2798b13.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-c29f4002.js";import{H as t}from"./Hero-ee4c8663.js";/* empty css             */function p(){return o.jsx(t,{title:"Autopůjčovna",img:"autopujcovna"})}export{p as default};
---
+++ assets/Hero-1d456f84.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-c8724db5.js";/* empty css             */function a({title:e,img:r}){return s.jsxs("div",{className:"hero",children:[s.jsx("h2",{className:"hero__title",children:e}),r&&s.jsx("img",{className:"hero__img",src:`./src/assets/${r}.png`})]})}export{a as H};
